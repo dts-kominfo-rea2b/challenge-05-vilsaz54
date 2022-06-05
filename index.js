e605a1ff854a46d0d28779a1873b5e494eab0f17
vilsaz54 <arsalanalief@gmail.com>
2022-06-05 19:00:27 +0700

    try to solving

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sort) => {
+  let result = sort(data);
+  let sortArray= []
+  for(let i=0;i<result.length;i++){
+    sortArray.push(`${i+1}. ${result[i]}`);
+  }
+
+  return sortArray;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => data.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => data.sort().reverse();
+
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
